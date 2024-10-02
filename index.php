2b0445e3ef390c340dc1c98a3d28fb920d52f42b
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-02 01:34:29 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+?>
+    <body style="background-color: red;">
+    <h1 style="color: white; font-weight: bold;">Home</h1>
+ 
+
+<?php
+include "view-footer.php";
+?>
+</body>
