413f01cdde41ce2e3a9431259b8aaa314b3c3777
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-02 01:59:17 -0500

    Create artists.php

---
+++ artists.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("util-db.php");
+require_once("model-artists.php);
+
+$pageTitle = "Artists";
+include "view-header.php";
+$artists = selectArtists();
+include "view-artists.php";
+include "view-footer.php";
+?>
