6f75a6bca1fbd23516112b0b106d70a3991d5bbf
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-17 01:35:01 -0500

    Update model-labels.php

--- model-labels.php
+++ model-labels.php
@@ -1,1 +1,16 @@
+<?php
+function selectLabels() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT label_id, label_name, label_yearsestablished FROM `label`");
 
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
