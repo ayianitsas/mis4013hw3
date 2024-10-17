093ba8be2cdcf67e273239e7fc15b08d25ba9b2e
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-17 01:36:59 -0500

    Create view-labels.php

---
+++ view-labels.php
@@ -0,0 +1,26 @@
+<h1>Labels</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Year Established</th>
+    </tr>
+  </thead>
+    <tbody>
+<?php
+while ($artist = $artists->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $artist['artist_id']; ?></td>
+    <td><?php echo $artist['artist_name']; ?></td>
+    <td><?php echo $artist['artist_genre']; ?></td>
+  </tr>
+<?php
+}
+?>
+      
+    </tbody>
+  </table>
+</div>
