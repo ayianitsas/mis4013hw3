59d1187fa26257e7404444e9639121e7501b6324
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-17 02:01:40 -0500

    Update model-labels-for-artists.php

--- model-labels-for-artists.php
+++ model-labels-for-artists.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-labels.php");
 
+$pageTitle = "Labels";
+include "view-header.php";
+$labels = selectLabels();
+include "view-labels.php";
+include "view-footer.php";
+?>
