4b882f9d67fbee9081872341e1a4a0b6da13a20a
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-19 03:24:44 -0500

    Create model-albums-by-genre.php

---
+++ model-albums-by-genre.php
@@ -0,0 +1,32 @@
+<?php
+function selectAlbums() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT artist_id, artist_name, artist_genre FROM `artist`");
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectLabelsForArtists($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.label_id, label_name, label_yearestablished, title, year FROM `label` c join album s on s.label_id = c.label_id where s.artist_id=?");
+        $stmt->bind_param("i", $aid);
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
