f24cfbbed2f64427b401923aaccf0710cc66ed1a
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-19 02:59:36 -0500

    Create genres.php

---
+++ genres.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-genres.php");
+
+$pageTitle = "Genres";
+include "view-header.php";
+$genres = selectGenres();
+include "view-genres.php";
+include "view-footer.php";
+?>
