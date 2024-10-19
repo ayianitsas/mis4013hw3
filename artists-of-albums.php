5b821882d0f8658a2fd62a3c7418d451ddd5775f
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-19 03:48:11 -0500

    Update and rename artists-for-albums.php to artists-of-albums.php

---
+++ artists-of-albums.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-artists-of-albums.php");
+
+$pageTitle = "Artists of Albums";
+include "view-header.php";
+$artists = selectArtistsOfAlbums($_GET['id']);
+include "view-artists-of-albums.php";
+include "view-footer.php";
+?>
