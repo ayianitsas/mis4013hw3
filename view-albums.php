608d473aa94c2d45d8687881427cb7df15fbd4ea
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-19 03:16:57 -0500

    Create view-albums.php

---
+++ view-albums.php
@@ -0,0 +1,28 @@
+<h1>Albums</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+    <th>ID</th>
+    <th>Title</th>
+    <th>Year</th>
+      <th></th>
+      
+    </tr>
+  </thead>
+    <tbody>
+<?php
+while ($album = $albums->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $album['album_id']; ?></td>
+    <td><?php echo $album['title']; ?></td>
+    <td><?php echo $album['year']; ?></td>
+  </tr>
+<?php
+}
+?>
+      
+    </tbody>
+  </table>
+</div>
