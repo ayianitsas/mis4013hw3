35f7aaa880e0613bb7e98e423c004a67b92ca6ce
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-19 02:02:12 -0500

    Update view-artists-with-labels.php

--- view-artists-with-labels.php
+++ view-artists-with-labels.php
@@ -8,13 +8,16 @@ while ($artist = $artists->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $artist['artist_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
   $labels = selectLabelsForArtists($artist['artist_id']);
   while ($label = $labels->fetch_assoc()) {
 ?>
+    <li class="list-group-item"><?php echo $label['label_name']; ?> - <?php echo $label['label_yearestablished']; ?></li>
 <?php    
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Genre: <?php echo $artist['artist_genre']; ?></small></p>
     </div>
