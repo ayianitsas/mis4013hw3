11e0e5d93c03661e2cc97ac99e82f99d919313b9
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-19 03:04:31 -0500

    Create view-genres.php

---
+++ view-genres.php
@@ -0,0 +1,28 @@
+<h1>Artists</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+    <th>ID</th>
+    <th>Genre</th>
+    <th>Popular Year</th>
+      <th></th>
+      
+    </tr>
+  </thead>
+    <tbody>
+<?php
+while ($genre = $genres->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $genre['genre_id']; ?></td>
+    <td><?php echo $genre['genre_name']; ?></td>
+    <td><?php echo $genre['popular_year']; ?></td>
+  </tr>
+<?php
+}
+?>
+      
+    </tbody>
+  </table>
+</div>
