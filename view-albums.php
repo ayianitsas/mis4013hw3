690eb14fa3cd99ae3d896a29828e0c41c98ebc4d
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-19 03:58:53 -0500

    Update view-albums.php

--- view-albums.php
+++ view-albums.php
@@ -28,35 +28,3 @@ while ($album = $albums->fetch_assoc()) {
   </table>
 </div>
 
-
-
-
-<h1>Artists</h1>
-<div class="table-responsive">
-  <table class="table">
-  <thead>
-    <tr>
-    <th>ID</th>
-    <th>Name</th>
-    <th>Genre</th>
-      <th></th>
-      
-    </tr>
-  </thead>
-    <tbody>
-<?php
-while ($artist = $artists->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $artist['artist_id']; ?></td>
-    <td><?php echo $artist['artist_name']; ?></td>
-    <td><?php echo $artist['artist_genre']; ?></td>
-    <td><a href="labels-for-artists.php?id=<?php echo $artist['artist_id']; ?>">Labels</a></td>
-  </tr>
-<?php
-}
-?>
-      
-    </tbody>
-  </table>
-</div>
