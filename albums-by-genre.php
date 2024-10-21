6859bb5d390fd1bdbf50b1a5f8efb4189effca6a
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-21 00:28:12 -0500

    Update albums-by-genre.php

--- albums-by-genre.php
+++ albums-by-genre.php
@@ -4,7 +4,7 @@ require_once("model-albums-by-genre.php");
 
 $pageTitle = "Albums by Genre";
 include "view-header.php";
-$albums = selectAlbums();
+$albums = selectAlbumsByGenre();
 include "view-albums-by-genre.php";
 include "view-footer.php";
 ?>
