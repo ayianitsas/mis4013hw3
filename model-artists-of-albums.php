747431891c0d8cd8727924f05bd393377704d695
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-21 01:28:06 -0500

    Update model-artists-of-albums.php

--- model-artists-of-albums.php
+++ model-artists-of-albums.php
@@ -1,9 +1,9 @@
 <?php
-function selectArtistsOfAlbums($i) {
+function selectArtistsOfAlbums($aid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT artist_id, artist_name, artist_genre FROM `artist` join Genres on artist_id = artist_id");
-        $stmt->bind_param("i", 1);
+        $stmt->bind_param("I", $aid);
 
         $stmt->execute();
         $result = $stmt->get_result();
