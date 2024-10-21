2affc211f423a7f866e933b5d423f5e304e5e43a
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-21 00:53:15 -0500

    Update model-artists-of-albums.php

--- model-artists-of-albums.php
+++ model-artists-of-albums.php
@@ -2,7 +2,7 @@
 function selectArtistsOfAlbums($aid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.artist_id, artist_name, artist_genre FROM `artist` c join genre s on s.artist_id = c.artist_id where s.album_id=?");
+        $stmt = $conn->prepare("SELECT c.artist_id, artist_name, artist_genre FROM `artist` c join Genres s on s.artist_id = c.artist_id where s.album_id=?");
         $stmt->bind_param("i", $aid);
 
         $stmt->execute();
@@ -17,3 +17,4 @@ function selectArtistsOfAlbums($aid) {
 ?>
 
 <!-- SELECT c.label_id, label_name, label_yearestablished, title, year FROM `label` c join album s on s.label_id = c.label_id where s.artist_id=?
+
