e39b39b2fed36772c3e839fbe3deffaeb56d437b
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-21 00:32:55 -0500

    Update model-albums-by-genre.php

--- model-albums-by-genre.php
+++ model-albums-by-genre.php
@@ -1,4 +1,19 @@
 <?php
+function selectAlbums() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT album_id, title, year FROM `album`");
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function selectAlbumsByGenre($aid) {
     try {
         $conn = get_db_connection();
@@ -15,3 +30,6 @@ function selectAlbumsByGenre($aid) {
     }
 }
 ?>
+
+
+
