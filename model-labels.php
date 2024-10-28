a03dda23033d7c0f1f4111fbf07a56649fcd3221
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-10-28 13:16:42 -0500

    Update model-labels.php

--- model-labels.php
+++ model-labels.php
@@ -13,7 +13,7 @@ function selectLabels() {
         throw $e;
     }
 }
-?>
+
 
 function insertLabel($cLName, $cYearEst) {
     try {
@@ -21,11 +21,41 @@ function insertLabel($cLName, $cYearEst) {
         $stmt = $conn->prepare("INSERT INTO `label` (`label_name`, `label_yearestablished`) VALUES (?, ?)");
         $stmt->bind_param("ss", $cLName, $cYearEst);
         $success = $stmt->execute();
-        $result = $stmt->get_result();
         $conn->close();
-        return $result;
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
     }
 }
+
+
+function updateLabel($cLName, $cYearEst, $lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `label` set `label_name` = ?, `label_yearestablished` = ? where label_id = ?");
+        $stmt->bind_param("ssi", $cLName, $cYearEst, $lid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteLabel($lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from label where label_id=?");
+        $stmt->bind_param("i", $lid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+>?
