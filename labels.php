7cb090e41519859898e402ba7d54314ab75bec5b
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-11-11 13:46:24 -0600

    Update labels.php

--- labels.php
+++ labels.php
@@ -4,6 +4,15 @@ require_once("model-labels.php");
 
 $pageTitle = "Labels";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType]) {
+    case "Add":
+      insertLabel($_POST['cName'], $_POST['cYear']);
+      break;
+  }
+}
+
 $labels = selectLabels();
 include "view-labels.php";
 include "view-footer.php";
