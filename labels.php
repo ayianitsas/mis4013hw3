2fa1cbf37e7a73b8d52a5e79bd7f024d3abcb994
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-11-12 22:19:56 -0600

    Update labels.php

--- labels.php
+++ labels.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertLabel($_POST['cName'], $_POST['cYear']);
+      if (insertLabel($_POST['cName'], $_POST['cYear'])) {
+        echo '<div class="alert alert-success" role="alert">Label added!</div>"';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>"';
+      }
       break;
   }
 }
