ad8a9a1851340bbdf783ee7203d5cbf35f22858d
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-11-21 01:16:40 -0600

    Update view-artists-input-list.php

--- view-artists-input-list.php
+++ view-artists-input-list.php
@@ -1,1 +1,9 @@
-
+<select class="form-select" id="cName" name="cName">
+ <?php
+while ($artistItem = $artistList->fetch_assoc()) {
+?>
+    <option value="<?php echo $artistItem['artist_id']; ?>"><?php echo $artistItem['artist_name']; ?></option>
+<?php
+}
+?>
+</select>
