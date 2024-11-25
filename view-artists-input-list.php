a41ba948db0e8a6eddfa9d33a0fab9d0fa2ac21a
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-11-25 13:44:58 -0600

    Update view-artists-input-list.php

--- view-artists-input-list.php
+++ view-artists-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="cName" name="cName">
  <?php
 while ($artistItem = $artistList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedArtist == $artistItem['artist_id']) {
+   $selText = " selected";
+  }
 ?>
-    <option value="<?php echo $artistItem['artist_id']; ?>"><?php echo $artistItem['artist_name']; ?></option>
+    <option value="<?php echo $artistItem['artist_id']; ?>"<?=$selText?>><?php echo $artistItem['artist_name']; ?></option>
 <?php
 }
 ?>
