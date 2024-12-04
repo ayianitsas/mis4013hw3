82439334d212baf351ebe7dea062969300e54139
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-04 02:40:18 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -54,6 +54,6 @@
 </nav>
 
         <header
-          style="background-image: linear-gradient(to right, #CDACB1, #D7BDC1, #DFCACD); padding: 20px; text-align: center;">
+          style="background-image: linear-gradient(to right, #C1979D,#CDACB1, #D7BDC1, #DFCACD); padding: 20px; text-align: center;">
       
     </header>
