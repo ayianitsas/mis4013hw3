7fa73a47e76370f58864ce20f3e8b1335179433f
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-04 00:01:14 -0600

    Update view-artists-with-labels-newform.php

--- view-artists-with-labels-newform.php
+++ view-artists-with-labels-newform.php
@@ -1,49 +1,46 @@
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newLabelModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
     </svg>
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="newLabelModal" tabindex="-1" aria-labelledby="newLabelModelLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newLabelModalLabel">New Label</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-  
-
-<form method="post" action="">
-
-
-        <div class="mb-3">
-            <label for="cName" class="form-label">Artist Name</label>
-<?php
-$artistList = selectArtistsForInput();
-$selectedArtist = 0;
-include "view-artists-input-list.php";
-?>
-          </div>
-
-   <div class="mb-3">
-            <label for="cYear" class="form-label">Label Name</label>
-            <input type="text" class="form-control" id="cYear" name="cYear">
-          </div>
-
-        
-        
-        <div class="mb-3">
-            <label for="cYear" class="form-label">Year Established</label>
-            <input type="text" class="form-control" id="cYear" name="cYear">
-          </div>
-            <input type="hidden" name="actionType" value="Add">
-          <button type="submit" class="btn btn-primary">Save</button>
-        </form>
-      </div>
+<div class="modal fade" id="newLabelModal" tabindex="-1" aria-labelledby="newLabelModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="newLabelModalLabel">New Label</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Form with action for Artists with labels page -->
+                <form method="post" action="/artists-with-labels">
+                    <div class="mb-3">
+                        <label for="artistSelect" class="form-label">Artist Name</label>
+                        <?php
+                        $artistList = selectArtistsForInput();
+                        echo '<select class="form-control" id="artistSelect" name="artistId">';
+                        foreach ($artistList as $artist) {
+                            echo '<option value="' . $artist['id'] . '">' . htmlspecialchars($artist['name']) . '</option>';
+                        }
+                        echo '</select>';
+                        ?>
+                    </div>
+                    <div class="mb-3">
+                        <label for="labelName" class="form-label">Label Name</label>
+                        <input type="text" class="form-control" id="labelName" name="labelName" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="establishedYear" class="form-label">Year Established</label>
+                        <input type="number" class="form-control" id="establishedYear" name="establishedYear" required>
+                    </div>
+                    <input type="hidden" name="actionType" value="Add">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
+
