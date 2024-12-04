c968fce90f78261a3931db7901fd094ee88d12e1
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-04 12:46:34 -0600

    Update artists.php

--- artists.php
+++ artists.php
@@ -7,4 +7,8 @@ include "view-header.php";
 $artists = selectArtists();
 include "view-artists.php";
 include "view-footer.php";
+
+
+<table style="background-color: #9D5D67; color: black; border-collapse: collapse; width: 100%;">
+
 ?>
