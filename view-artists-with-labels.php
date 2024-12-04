5a63e037ccbb15b5a3be689235240a750f98adc5
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-04 12:50:09 -0600

    Update view-artists-with-labels.php

--- view-artists-with-labels.php
+++ view-artists-with-labels.php
@@ -10,7 +10,6 @@ include "view-artists-with-labels-newform.php";
 </div>
   <div class="card-group">
 
- <card style="background-color: #B17D85; color: black; border-collapse: collapse; width: 100%;">
 
     
 <?php
