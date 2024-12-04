5ba51c2a0ccdf49339d04540eba9c240461af12c
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-04 13:06:38 -0600

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -7,8 +7,10 @@
   <thead>
     <tr>
     <the><strong>ID</strong></th>
-    <th><strong>Name</strong></th>
-    <th><strong>Genre</strong></th>
+
+      <th style="text-align: left;"><strong>Name</strong></th>
+<th style="text-align: left;"><strong>Genre</strong></th>
+
       <th></th>
       
     </tr>
