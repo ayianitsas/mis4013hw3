c5e0ece3f60ce07e0eb32b231dff070aed0566b8
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-04 00:00:03 -0600

    Update view-artists-with-labels-newform.php

--- view-artists-with-labels-newform.php
+++ view-artists-with-labels-newform.php
@@ -16,7 +16,9 @@
       </div>
       <div class="modal-body">
   
-        <form method="post" action="artists-with-labels">
+
+<form method="post" action="">
+
 
         <div class="mb-3">
             <label for="cName" class="form-label">Artist Name</label>
