d60e0118697902d95c7bf62902490900164551dc
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-10 00:55:32 -0600

    Update genres.php

--- genres.php
+++ genres.php
@@ -4,7 +4,41 @@ require_once("model-genres.php");
 
 $pageTitle = "Genres";
 include "view-header.php";
+
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertGenre($_POST['gName'], $_POST['gPopYear'])) {
+        echo '<div class="alert alert-success" role="alert">Genre added!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  case "Edit":
+      if (updateGenre($_POST['gName'], $_POST['gPopYear'], $_POST['gid'])) {
+        echo '<div class="alert alert-success" role="alert">Genre edited!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  case "Delete":
+      if (deleteGenre($_POST['gid'])) {
+        echo '<div class="alert alert-success" role="alert">Genre deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+
+
+
 $genres = selectGenres();
 include "view-genres.php";
 include "view-footer.php";
 ?>
+
+
+
