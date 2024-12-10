fc13c6236115fbb41242ddfe4eb7f661b0648d22
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-10 00:13:22 -0600

    Update model-albums.php

--- model-albums.php
+++ model-albums.php
@@ -22,7 +22,6 @@ function insertAlbum($aTitle, $aYear) {
          $stmt->bind_param("ss", $aTitle, $aYear);
 
         $success = $stmt->execute();
-        mysqli_stmt->execute()
       
         $conn->close();
         return $success;
