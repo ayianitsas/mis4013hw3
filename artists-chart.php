2c1b2ac6078d09b0679937b0b922e8c8fc6d271e
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-11 23:16:44 -0600

    Update artists-chart.php

--- artists-chart.php
+++ artists-chart.php
@@ -5,6 +5,6 @@ require_once("model-artists-chart-db.php");
 $pageTitle = "Artists Chart";
 include "view-header.php";
 $artists = selectArtists();
-include "view-artists-page.php";
+include "view-artists-chart.php";
 include "view-footer.php";
 ?>
