f1f107fa4ed25451689571b6a5ec32633f796838
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-11 23:18:29 -0600

    Update model-artists-chart-db.php

--- model-artists-chart-db.php
+++ model-artists-chart-db.php
@@ -2,8 +2,10 @@
 function selectArtists() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT artist_name, count(a.album_id) as num_albums FROM 'artist' r join album a on a.album_id=r.artist_id group by artist_name");
-
+        $stmt = $conn->prepare("SELECT r.artist_name, COUNT(a.album_id) AS num_albums
+                        FROM artist r
+                        JOIN album a ON a.artist_id = r.artist_id
+                        GROUP BY r.artist_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
