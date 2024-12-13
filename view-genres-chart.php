bbc36765dd27b4d3bc7a7f61927566442bca3525
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-12 23:55:57 -0600

    Create view-genres-chart.php

---
+++ view-genres-chart.php
@@ -0,0 +1,37 @@
+<h1>Labels By Genre</h1>
+<div style="width: 400px; height: 400px;">
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+    datasets: [{
+        data: [
+<?php
+while ($genre = $genres->fetch_assoc()) {
+      echo $genre['num_labels'] . ", ";
+}
+?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$genres = selectGenres();
+while ($genre = $genres->fetch_assoc()) {
+      echo "'" . $genre['genre_name'] . "', ";
+}
+?>
+    ]
+},
+  });
+</script>
