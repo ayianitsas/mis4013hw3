763151a5361b8113ffa864aefc4c8aca58497b41
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 00:35:23 -0600

    Update view-albums-chart.php

--- view-albums-chart.php
+++ view-albums-chart.php
@@ -17,8 +17,8 @@
     datasets: [{
         data: [
 <?php
-while ($artist = $artists->fetch_assoc()) {
-      echo $artist['num_labels'] . ", ";
+while ($album = $albums->fetch_assoc()) {
+      echo $album['num_albums'] . ", ";
 }
 ?>
         ]
@@ -27,9 +27,9 @@ while ($artist = $artists->fetch_assoc()) {
     // These labels appear in the legend and in the tooltips when hovering different arcs
     labels: [
 <?php
-$artists = selectArtists();
-while ($artist = $artists->fetch_assoc()) {
-      echo "'" . $artist['artist_name'] . "', ";
+$albums = selectAlbums();
+while ($album = $albums->fetch_assoc()) {
+      echo "'" . $album['album_name'] . "', ";
 }
 ?>
     ]
