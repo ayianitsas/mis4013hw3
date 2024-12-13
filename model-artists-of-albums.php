5fa5ad7322a002c5f2fb84334b873a9a34fa4aba
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 01:25:53 -0600

    Update model-artists-of-albums.php

--- model-artists-of-albums.php
+++ model-artists-of-albums.php
@@ -16,7 +16,3 @@ function selectArtistsOfAlbums($aid) {
 }
 ?>
 
-<!-- SELECT c.label_id, label_name, label_yearestablished, title, year FROM `label` c join album s on s.label_id = c.label_id where s.artist_id=?
-<!-- 
-
-<!-- SELECT artist_id, artist_name, artist_genre FROM `artist` join Genres on artist_id = artist_id
