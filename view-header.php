b27a08d8174854a3f1219ae708c511bc6d012df1
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-12 23:56:28 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -46,6 +46,10 @@
          <li class="nav-item">
            <a class="nav-link" href="artists-chart.php">Artists Chart</a>
          </li>
+
+         <li class="nav-item">
+           <a class="nav-link" href="genres-chart.php">Genres Chart</a>
+         </li>
       </ul>
     </div>
   </div>
