8d1f9d417373f807af3aecad0be17e43d1e5bf1e
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 01:04:58 -0600

    Update labels-chart.php

--- labels-chart.php
+++ labels-chart.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-labels-chart-db.php");
 
-$pageTitle = "Albums Chart";
+$pageTitle = "Labels Chart";
 include "view-header.php";
 $labels = selectLabels();
 include "view-labels-chart.php";
