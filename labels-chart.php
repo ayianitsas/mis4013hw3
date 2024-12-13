6f1744981bc214c4a8c33a53e8321d77d801637a
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 00:58:05 -0600

    Create labels-chart.php

---
+++ labels-chart.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-labels-chart-db.php");
+
+$pageTitle = "Albums Chart";
+include "view-header.php";
+$labels = selectLabels();
+include "view-labels-chart.php";
+include "view-footer.php";
+?>
