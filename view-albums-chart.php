19a6811899a495314a66f481bfd9a60ab9bab4e8
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 00:29:51 -0600

    Create view-albums-chart.php

---
+++ view-albums-chart.php
@@ -0,0 +1,38 @@
+<h1>Albums Per Artist</h1>
+<div style="width: 400px; height: 400px;">
+  <canvas id="myChart"></canvas>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+<?php
+while ($artist = $artists->fetch_assoc()) {
+      echo $artist['num_labels'] . ", ";
+}
+?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$artists = selectArtists();
+while ($artist = $artists->fetch_assoc()) {
+      echo "'" . $artist['artist_name'] . "', ";
+}
+?>
+    ]
+},
+  });
+</script>
