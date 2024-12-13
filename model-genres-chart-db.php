085288fcdb6f1eb1243d9d134d779f8e538cf95b
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 01:00:07 -0600

    Update model-genres-chart-db.php

--- model-genres-chart-db.php
+++ model-genres-chart-db.php
@@ -4,10 +4,6 @@ function selectGenres() {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT g.genre_name, COUNT(l.label_id) AS num_labels FROM Genres g JOIN label l ON genre_id = g.genre_id GROUP BY g.genre_id, g.genre_name;");
 
-
-
-
-        
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
