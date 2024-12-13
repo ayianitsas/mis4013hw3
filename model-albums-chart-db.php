e69fc6b985934059be154f46aa4e4d6d9587e676
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-13 01:00:18 -0600

    Update model-albums-chart-db.php

--- model-albums-chart-db.php
+++ model-albums-chart-db.php
@@ -4,10 +4,6 @@ function selectAlbums() {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT t.title, COUNT(l.label_id) AS num_labels FROM album t JOIN label l ON album_id = t.album_id GROUP BY t.album_id, t.title;");
 
-
-
-
-        
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
