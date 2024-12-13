aa642604a048f4e6fbf1bbc418b8b76e6b97aaec
ayianitsas <157073211+ayianitsas@users.noreply.github.com>
2024-12-12 23:47:29 -0600

    Update genres-chart.php

--- genres-chart.php
+++ genres-chart.php
@@ -1,10 +1,12 @@
 <?php
 require_once("util-db.php");
-require_once("model-genres.php");
+require_once("model-genres-chart-db.php");
 
-$pageTitle = "Genres";
+$pageTitle = "Genres Chart";
 include "view-header.php";
 $genres = selectGenres();
-include "view-genres.php";
+include "view-genres-chart.php";
 include "view-footer.php";
 ?>
+
+
